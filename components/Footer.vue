7448cc137ce4df4591f35c6ff9e8308014a8b0d0
海崎世山 <deversoftyrecord@gmail.com>
2023-03-02 11:25:31 +0700

    Add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,26 @@
 <template>
-    <div class="" id="footer">
+    <div class="border-t" id="footer">
         <div class="" id="footer-body">
-            <div class="w-full max-w-screen-xl my-0 mx-auto py-20 px-0" id="footer-center">
-                <div class="" id="footer-col1">
-                
+            <div class="flex w-full mx-auto my-0 px-20 py-0 max-w-screen-xl" id="footer-center">
+                <div class="flex pl-0 " id="footer-col">
+                    <div class="" id="footer-box">
+                        <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_dark_color_272x92dp.png" class="h-8 mr-4" alt="Logo">
+                    </div>
+
+                    <div class="" id="footer-item">
+                        <div class="" id="footer-menu">
+                            <nuxtlink class="mb-3.5 text-sm font-bold font text-gray-600" to="#">Abc</nuxtlink>
+                            <nuxtlink class="mb-3.5 text-sm font-bold text-gray-600" to="#">Abc</nuxtlink>
+                            <nuxtlink class="mb-3.5 text-sm font-bold text-gray-600" to="#">Abc</nuxtlink>
+                            <nuxtlink class="mb-3.5 text-sm font-bold text-gray-600" to="#">Abc</nuxtlink>
+                            <nuxtlink class="mb-3.5 text-sm font-bold text-gray-600" to="#">Abc</nuxtlink>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="" id="footer-bottom">
+        
         </div>
     </div>
 </template>
